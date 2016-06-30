2f6e604a0229af2d1d612c695d987c362036f25d
Adrian Rincon <adrian_rm_99@hotmail.com>
2016-06-30 17:00:45 -0400

    Proyecto para cambiar imagen

---
+++ js/images-changer.js
@@ -0,0 +1,12 @@
+var myImage = document.querySelector('img');
+//funcion al hacer click en objeto contenido en myImage
+myImage.onclick = function(){
+	//obtener el valor del atributo 'src' del objeto myImage
+	var mySrc = myImage.getAttribute('src');
+	if (mySrc==='images/1.jpg'){
+		myImage.setAttribute('src','images/2.jpg');
+	}
+	else{
+		myImage.setAttribute('src','images/1.jpg');
+	}
+}
