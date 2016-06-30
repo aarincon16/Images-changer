167c99b2c00bdb26efe482ec0cd30e81be6b05f8
Adrian Rincon <adrian_rm_99@hotmail.com>
2016-06-30 17:05:21 -0400

    Cambio de onclik a onmouseover

--- js/images-changer.js
+++ js/images-changer.js
@@ -1,6 +1,6 @@
 var myImage = document.querySelector('img');
 //funcion al hacer click en objeto contenido en myImage
-myImage.onclick = function(){
+myImage.onmouseover = function(){
 	//obtener el valor del atributo 'src' del objeto myImage
 	var mySrc = myImage.getAttribute('src');
 	if (mySrc==='images/1.jpg'){
